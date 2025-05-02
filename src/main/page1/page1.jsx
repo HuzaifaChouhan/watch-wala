adbfad5b0035d11bcf5f32cc8d5548b496b54426
HuzaifaChouhan <huzefachouhan07@gmail.com>
2025-05-02 16:46:47 +0530

    main page done

--- src/main/page1/page1.jsx
+++ src/main/page1/page1.jsx
@@ -14,11 +14,11 @@ export default function Page1(){
                 </div>
                 <div className="page1-r">
                     <img src={diamondsvg} alt="diamondsvg" className='svg'/>
-                    <p className='page1-start-r1'>You dont even have to speak your wrist does the talking.</p>
+                    <p className='page1-start-r1'>You dont even have to <br/> speak your wrist does the talking.</p>
                 </div>
                 <div className="page1-r">
                     <img src={moneysvg} alt="moneysvg" className='svg'/>
-                    <p className='page1-start-r1'>Its something you can pass down, not just wear.</p>
+                    <p className='page1-start-r1'>Its something you can pass <br/> down, not just wear.</p>
                 </div>
             </div>
             <div className="page1-img-box">
--- src/main/page2/page2.jsx
+++ src/main/page2/page2.jsx
@@ -1,16 +1,21 @@
 import './page2.css';
 import Component from './component/component';
-import watch1 from '../../images/watch1.jpg';
+import Rolex1 from '../../images/Rolex-1.jpeg';
+import Hublot1 from '../../images/Hublot-1.jpeg';
+import Gshock1 from '../../images/G-steel-1.jpeg';
+import Cartier1 from '../../images/Cartier-1.jpeg';
+import PatekPhillipe1 from '../../images/Patek Phillipe-1.jpeg'
+import Tissot1 from '../../images/Tissot-1.jpeg';
 
 export default function Page2(){
     return(
         <div className="page2">
-            <Component photo={watch1} name="Rolex"/>
-            <Component photo={watch1} name="Rolex"/>
-            <Component photo={watch1} name="Rolex"/>
-            <Component photo={watch1} name="Rolex"/>
-            <Component photo={watch1} name="Rolex"/>
-            <Component photo={watch1} name="Rolex"/>
+            <Component photo={Rolex1} name="Rolex"/>
+            <Component photo={Hublot1} name="Hublot"/>
+            <Component photo={Gshock1} name="G-shock"/>
+            <Component photo={Cartier1} name="Cartier"/>
+            <Component photo={PatekPhillipe1} name="Patek Phillipe"/>
+            <Component photo={Tissot1} name="Tissot"/>
         </div>
     );
 }
